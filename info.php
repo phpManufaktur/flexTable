fc61fa275ee2793b0238fbe30d9c2c47d2bbc8a6
hertsch <ralf.hertsch@phpmanufaktur.de>
2012-02-16 08:31:28 +0000

    Release 0.21
    git-svn-id: https://server3.subversion-server.com/phpmanufaktur/svn/flexTable/trunk@29 a5cc2e76-e61d-9947-bb2d-1e5ecedd38e7

--- info.php
+++ info.php
@@ -2,13 +2,13 @@
 
 /**
  * flexTable
- * 
+ *
  * @author Ralf Hertsch (ralf.hertsch@phpmanufaktur.de)
  * @link http://phpmanufaktur.de
  * @copyright 2011
  * @license GNU GPL (http://www.gnu.org/licenses/gpl.html)
  * @version $Id$
- * 
+ *
  * FOR VERSION- AND RELEASE NOTES PLEASE LOOK AT INFO.TXT!
  */
 
@@ -34,12 +34,12 @@ if (defined('WB_PATH')) {
 $module_directory     = 'flex_table';
 $module_name          = 'flexTable';
 $module_function      = 'tool';
-$module_version       = '0.20';
+$module_version       = '0.21';
 $module_status        = 'Stable';
-$module_platform      = '2.8'; 
+$module_platform      = '2.8';
 $module_author        = 'Ralf Hertsch, Berlin (Germany)';
 $module_license       = 'GNU General Public License';
-$module_description   = 'Flexible table module for WebsiteBaker'; 
+$module_description   = 'Flexible table module for WebsiteBaker';
 $module_home          = 'http://phpmanufaktur.de/flex_table';
 $module_guid          = 'E47DC7FE-5511-4BF9-91D7-148416EB796E';
 
